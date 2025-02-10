d2d400b037baed119d764380166b7333daae9b0d
dani-agudelo <daniiagudelom@gmail.com>
2025-02-10 13:56:53 -0500

    create, edit, and delete a task in a basic way

--- src/components/EditTodoForm.jsx
+++ src/components/EditTodoForm.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const EditTodoForm = () => {
+export const EditTodoForm = ({ editTodo, task}) => {
+  const [value, setValue] = useState(task.task) // se pone que no sea un valor por defecto para que al editar veamos qué había
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    editTodo(value, task.id)
+
+    setValue("") // para borrar y poner una nueva tarea
+  }
+
+
   return (
-    <div>EditTodoForm</div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type="text" className='todo-input'
+        value={value}
+        placeholder='Editar tarea' onChange={(e) => setValue(e.target.value)} />
+      <button type='submit' className='todo-btn'>Editar
+      </button>
+    </form>
   )
 }
